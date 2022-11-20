852acd43ad2ad72310ed2a4da1de16c02d24c633
Narayanan M.S <107301043+Narenweb@users.noreply.github.com>
2022-11-20 16:25:55 +0530

    Update Upload.js

--- login/src/Components/Upload.js
+++ login/src/Components/Upload.js
@@ -1,13 +1,72 @@
 import React from "react";
 
+
+
+
+
 function Upload(){
-    console.log("hello");
+   
     return(
         <div>
-          <h1>hello</h1>
+        <h2>Enter details</h2>
+          <div className="form-group">
+                <label>Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Full Name"
+                  name="name"
+                 
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Amount</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Amount"
+                
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Amount in words</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Amount in words"
+                 
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Date</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Enter Date"
+                  
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Account Number</label>
+                <input
+                  type="Phone"
+                  className="form-control"
+                  placeholder="Enter Accoune Number"
+                  
+                />
+              </div>
+              
+                <button type="submit" className="btn btn-dark btn-lg btn-block button1 ">
+                Submit
+              </button>
+              
         </div>
     );
 }
 
 
-export default Upload();
+export default Upload;
